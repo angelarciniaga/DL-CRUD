740e5bf1127138d2f12ff12139cb5412f4f2835f
Your Name <you@example.com>
2020-12-09 00:18:55 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{name: 'Usuario'}">Usuario</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Inicio.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
+      <template #header>Desafío</template>
+
+      <template #lead>
+        Desafío - Creación de CRUD mediante Vuex
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        Crear un CRUD, un sistema que pueda crear un usuario, editar usuarios, eliminar usuarios y listar los usuarios, hecho con dos componentes, uno con un formulario para el envío de datos, el otro listar los usuarios con un botón en cada una de las filas de usuarios, para eliminar dicho usuario, este sistema se conectará a una api que se conecta a la base de datos de Firebase a través de cloud functions. Hecho con ayuda de vuex.
+
+      </p>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Inicio',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
