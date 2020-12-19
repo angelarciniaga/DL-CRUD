223921017e601579b6b69221533f62d5cf552d5f
Your Name <you@example.com>
2020-12-18 23:02:52 -0300

    agregando el acrchivo config de firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDx1MeBstQtXtegsXEig13f8IRiLuMWbeM",
+    authDomain: "dl-crud-97d02.firebaseapp.com",
+    projectId: "dl-crud-97d02",
+    storageBucket: "dl-crud-97d02.appspot.com",
+    messagingSenderId: "296569772517",
+    appId: "1:296569772517:web:feea5953d2da907418f70c",
+    measurementId: "G-LR3NWDDL5D"
+};
